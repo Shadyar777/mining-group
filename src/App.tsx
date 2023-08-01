34fe8c0167d66621aff6ff000027dc6ac6d3f477
romishkuvatov <romish@mykeruen.kz>
2023-08-01 21:54:50 +0600

    pushed the footer to the bottom of the page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
   const element = useAppRoutes();
 
   return (
-    <Box display='flex' flexDirection='column' height='100%'>
+    <Box display='flex' flexDirection='column' minHeight='100vh'>
       <Header />
       <Box flex='1 0 auto'>{element}</Box>
       <Footer />
