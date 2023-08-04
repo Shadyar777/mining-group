6fc5374b31071f96a1cdd9f3929f523705a62673
Shadyar <sh.utegenov@vlife.kz>
2023-08-05 01:35:34 +0600

    Add filters

---
+++ src/modules/landingPage /assetsEnterprise/components/investor/FilterPopover.tsx
@@ -0,0 +1,130 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Checkbox,
+  FormControlLabel,
+  Popover,
+  styled,
+  Typography,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+
+type TFilterPopoverProps = {
+  anchorEl: HTMLElement | null;
+  handlePopoverClose: () => void;
+};
+
+const StyledPopover = styled(Popover)(({ theme: { breakpoints } }) => ({
+  '& .filter__common': {
+    color: 'inherit',
+    textAlign: 'center',
+    fontSize: '16px',
+    fontWeight: 300,
+  },
+  '& .MuiPopover-paper, .MuiAccordion-root': {
+    background: '#FFF8EC !important',
+  },
+  '& .Mui-expanded': {
+    margin: '0 !important',
+    height: '0% !important'
+  },
+  '& .MuiAccordionSummary-root, .MuiButtonBase-root': {
+    height: '0% !important'
+  },
+
+  '& .filter__publication-date': {
+    color: '#6A6A6A',
+    fontSize: '12px',
+    fontWeight: 400,
+  },
+  '& .filter__new-old': {
+    color: '#6A6A6A',
+    fontSize: '12px',
+    fontWeight: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+
+    '& > div': {
+      width: '100%',
+      padding: '3px 8px',
+      cursor: 'pointer',
+    },
+  },
+  // '& .filter__publication-date': {},
+  [breakpoints.down('sm')]: {},
+}));
+
+const FilterPopover = ({
+  anchorEl,
+  handlePopoverClose,
+}: TFilterPopoverProps) => {
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  const { register, watch } = useForm();
+
+  const watchAllFields = watch();
+
+  useEffect(() => {
+    console.log(watchAllFields);
+  }, [watchAllFields]);
+
+  return (
+    <div>
+      <StyledPopover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handlePopoverClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className='filter__common'>Сортировка</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography component={'span'} className='filter__publication-date'>
+              По дате публикации:
+            </Typography>
+            <div className='filter__new-old'>
+              <div>Новые</div>
+              <div>Старые</div>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className='filter__common'>
+              Полезные ископаемые
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <FormControlLabel
+              className='filter__common'
+              control={<Checkbox {...register('Ископаемое 1')} />}
+              label='Ископаемое 1'
+            />
+            <FormControlLabel
+              className='filter__common'
+              control={<Checkbox {...register('Ископаемое 2')} />}
+              label='Ископаемое 2'
+            />
+          </AccordionDetails>
+        </Accordion>
+      </StyledPopover>
+    </div>
+  );
+};
+
+export default FilterPopover;
--- src/modules/landingPage /assetsEnterprise/components/investor/MenuFilters.tsx
+++ src/modules/landingPage /assetsEnterprise/components/investor/MenuFilters.tsx
@@ -1,12 +1,19 @@
 import { styled } from '@mui/material';
+import React, { useState } from 'react';
+import TFilterPopoverProps from './FilterPopover.tsx';
+import SearchInput from "./SearchInput.tsx";
 
 const StyledMenuFilter = styled('div')(({ theme: { breakpoints } }) => ({
   display: 'flex',
   alignItems: 'center',
+  margin: '32px 0',
   justifyContent: 'space-between',
   '& .menu-filters': {
     display: 'flex',
     alignItems: 'center',
+    cursor: 'pointer',
+    gap: '0 16px',
+    marginRight: '90px',
   },
   '& .menu-filters__icon': {
     display: 'flex',
@@ -18,6 +25,7 @@ const StyledMenuFilter = styled('div')(({ theme: { breakpoints } }) => ({
     fontWeight: 400,
   },
   '& .menu-filters__search': {
+    width:'100%',
     borderRadius: '30px',
     background: 'rgba(255, 255, 255, 0.90)',
   },
@@ -28,16 +36,42 @@ const StyledMenuFilter = styled('div')(({ theme: { breakpoints } }) => ({
 }));
 
 const MenuFilters = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [searchValue, setSearchValue] = useState('');
+  const onOpenPopoverFilters = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const onClosePopoverFilter = () => {
+    // setAnchorEl(() => null)
+    // FIXME: Тут не понятный баг. Если использовать setState, почему-то значение не обнуляется
+    //  Пришлось обойти баг при помощью макрозадачей. Может быть и за batching-га реакта
+    setTimeout(() => {
+      setAnchorEl(() => null);
+    }, 20);
+  };
+
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+    // Дополнительные действия поиска, если нужно
+  };
+
   return (
     <StyledMenuFilter className='investor__menu-filters'>
-      <div className='menu-filters'>
+      <div className='menu-filters' onClick={onOpenPopoverFilters}>
         <div className='menu-filters__icon'>
           <img alt='' src='../../../../../../public/svgs/icon-filter.svg' />
         </div>
         <div className='menu-filters__label'>Фильтры</div>
+        <TFilterPopoverProps
+          anchorEl={anchorEl}
+          handlePopoverClose={onClosePopoverFilter}
+        />
       </div>
       <div className='menu-filters__search'>
-        <input type='text' />
+        <SearchInput value={searchValue} onChange={handleSearchChange} />
+
       </div>
     </StyledMenuFilter>
   );
