9a145483a1f1dae8db8e580de9f6cd085a0ac586
Shadyar <sh.utegenov@vlife.kz>
2023-08-13 19:05:45 +0600

    made a design on the SignIn page

--- src/modules/admin/signIn/SignIn.tsx
+++ src/modules/admin/signIn/SignIn.tsx
@@ -1,12 +1,88 @@
 import { useNavigate } from 'react-router-dom';
+import { Button, Grid, Paper, TextField, Typography, Box } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useForm } from 'react-hook-form';
+
+type Form = {
+  login: string;
+  password: string;
+};
 
 const SignIn = () => {
   const navigate = useNavigate();
   const goToAdmin = () => navigate('/admin/home', { replace: false });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>();
+
+  const onSubmit = (data: Form) => {
+    console.log(data);
+    goToAdmin();
+  };
+
   return (
-    <div>
-      <button onClick={goToAdmin}>sing in</button>
-    </div>
+    <Grid
+      container
+      justifyContent='center'
+      alignItems='center'
+      style={{ height: '100vh' }}
+    >
+      <Box component={Paper} elevation={3} p={4} minWidth={320}>
+        <Grid container direction='column' spacing={3} alignItems='center'>
+          <Grid item>
+            <LockOutlinedIcon fontSize='large' />
+          </Grid>
+          <Grid item>
+            <Typography variant='h5' gutterBottom>
+              Вход
+            </Typography>
+          </Grid>
+
+          <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%' }}>
+            <Box mb={2}>
+              <TextField
+                variant='outlined'
+                fullWidth
+                label='Логин'
+                {...register('login', {
+                  required: 'Логин обязателен',
+                  pattern: {
+                    value: /^[A-Za-z0-9]+$/i,
+                    message: 'Логин должен состоять только из букв и цифр',
+                  },
+                })}
+                error={Boolean(errors.login)}
+                helperText={errors.login && errors.login.message}
+              />
+            </Box>
+            <Box mb={2}>
+              <TextField
+                variant='outlined'
+                fullWidth
+                type='password'
+                label='Пароль'
+                {...register('password', {
+                  required: 'Пароль обязателен',
+                  minLength: {
+                    value: 6,
+                    message: 'Пароль должен содержать не менее 6 символов',
+                  },
+                })}
+                error={Boolean(errors.password)}
+                helperText={errors.password && errors.password.message}
+              />
+            </Box>
+            <Grid item>
+              <Button variant='contained' color='primary' type='submit'>
+                Войти
+              </Button>
+            </Grid>
+          </form>
+        </Grid>
+      </Box>
+    </Grid>
   );
 };
 
