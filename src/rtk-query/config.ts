13b0756d1d2d245dd0bee64045c37b70e4c83792
Shadyar <sh.utegenov@vlife.kz>
2023-09-16 21:03:09 +0600

    change api

--- src/rtk-query/config.ts
+++ src/rtk-query/config.ts
@@ -3,7 +3,7 @@ import { AppState } from '../store';
 import { getAddGlobalLanguages } from '../modules/common/sliceCommon/slice.ts';
 import { getAdmin } from '../modules/admin/signIn/slice.ts';
 
-export const baseUrl = 'http://13.49.241.81';
+export const baseUrl = 'http://195.49.212.117/api/';
 
 export function getBasicAuthHeader(login: string, password: string): string {
   const encodedCredentials = btoa(`${login}:${password}`);
